6998e42cc4458de576b25f6c278484fb482b068f
blumbeem <evan.blumberg21@gmail.com>
2026-03-06 21:20:11 -0500

    Refined Horizontal Grid breakpoints

--- frontend_template/components/firecalc/FireCalcForm.tsx
+++ frontend_template/components/firecalc/FireCalcForm.tsx
@@ -137,7 +137,7 @@ export default function FireCalcForm({
             </div>
 
             {/* Horizontal Grid */}
-            <div className="grid grid-cols-1 lg:grid-cols-2 2xl:grid-cols-4 gap-6">
+            <div className="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4 2xl:grid-cols-4 gap-4 xl:gap-6 items-start">
 
                 {/* Profile & Current Cash Flow - Combined for grid space */}
                 <div className="flex flex-col gap-6">
